0b73bdf6dfd0d6a9e5eac52b514e3c495ee6a77b
HeechangNam <45029647+HeechangNam@users.noreply.github.com>
2019-08-27 21:06:59 +0900

    Update 10817.py

--- 10817.py
+++ 10817.py
@@ -1,8 +1,9 @@
-def list_gen(lst=[]):
-    for i in lst:
-        yield int(i)
+#def list_gen(lst=[]):
+#    for i in lst:
+#        yield int(i)
 
 input_list = input().split()
-num_list = list(list_gen(input_list))
+#num_list = list(list_gen(input_list))
+num_list = [int(i) for i in input_list]
 num_list.sort()
 print(num_list[1])
