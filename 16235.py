a138b4ee39839a7f5046b7dc03cd2f5987538a4d
HeechangNam <45029647+HeechangNam@users.noreply.github.com>
2019-09-02 17:27:34 +0900

    Update 16235.py

--- 16235.py
+++ 16235.py
@@ -1,4 +1,8 @@
 class Tree:
+    '''나무 객체
+    live - 1이면 살고 0이면 죽은 것
+    age - 나이'''
+    
     def __init__(self,age):
         self.live = 1
         self.age = age
@@ -6,19 +10,23 @@ class Tree:
     def grow(self, nuriment):
         if nuriment < age:
             self.live = 0
+            return nuriment
         else:
             self.age += 1
+            return nuriment-age
     
     def death(self):
         if self.live == 0:
             return age//2
+        else:
+            return 0
     
     def seedable(self):
         if self.age%5 == 0:
             return True
         else:
             return False
-            
+        
 class Land:
     def __init__(self,nuriment):
         self.nuriment = nuriment
@@ -29,10 +37,6 @@ class Land:
         
     def tree_sort(self):
         self.tree.sort()
-        
-    def fertile(self):
-        for i in self.tree:
-            self.nuriment += i.death()
     
     def add_nuriment(nuriment):
         self.nuriment += nuriment
@@ -53,3 +57,36 @@ lands = [[Land(5) for j in range(n)] for i in range(n)]
 for i in range(m):
     x,y,age = map(int,input().split())
     lands[x-1][y-1].plant(age)
+    
+for year in range(k):
+    
+    #봄
+    for i in range(n):
+        for j in range(n):
+            lands[i][j].tree_sort()
+            for tree in lands[i][j].tree:
+                lands[i][j].nuriment = tree.grow(lands[i][j].nuriment)
+    
+    #for land in lands:
+    #    land.tree_sort()
+    #    for tree in land:
+    #        land.nuriment = tree.grow(land.nuriment)
+    #3중포문을 2중으로 바꿀 수 있지 않을까 싶었으나
+    #land가 Land객체 type이 아닌 list type으로 잡혀서 의미 없었음
+                
+    #여름
+    for i in range(n):
+        for j in range(n):
+            for tree in lands[i][j].tree:
+                lands[i][j].nuriment += tree.death()
+    
+    #for land in lands:
+    #    for tree in land:
+    #        land.nuriment += tree.death()
+    
+    #가을
+    for i in range(n):
+        for j in range(n):
+            for tree in lands[i][j].tree:
+                if tree.seedable():
+                    
